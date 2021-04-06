e575fcbe3a35b08e4e02dda9696955d4c5485604
sergejkoll <kolesnikov.school4@gmail.com>
2021-04-06 20:34:06 +0300

    fix 3 lab

--- lab3/main.py
+++ lab3/main.py
@@ -1,8 +1,6 @@
 from neuron import Neuron
 
 if __name__ == '__main__':
-    n = Neuron(4.5, 5, 4, 20, 0.1, 10000)
+    n = Neuron(4.5, 5, 2, 20, 0.5, 1000)
     n.learning()
-    n.plot_after_learning()
     n.predict()
-    n.plot_predict()
--- lab3/neuron.py
+++ lab3/neuron.py
@@ -19,87 +19,92 @@ class Neuron(object):
         self.N = N
         self.number_of_epochs = number_of_epochs
 
+        self.x_train = [a + i * (b - a) / N for i in range(N + 1)]
+        self.x_predict = [b + i * (b - a) / N for i in range(1, N)]
+        self.y_train = [self.f(x) for x in self.x_train]
+        self.y_predict = [self.f(x) for x in self.x_predict]
+
         self.weights = [0 for _ in range(size_p + 1)]
-        self.real_values = None
-        self.learning_values = None
 
-    def get_X(self, a=None, b=None, N=None) -> List:
-        points = []
-        if a is None and b is None:
-            step_in_interval = (self.b - self.a) / self.N
-            for i in range(self.N):
-                points.append(self.a + i * step_in_interval)
-        else:
-            step_in_interval = (b - a) / N
-            for i in range(N):
-                points.append(a + i * step_in_interval)
+    @staticmethod
+    def f(x):
+        return np.sin(2 * np.sqrt(np.exp(x)))
 
-        return points
+    def get_X_predict(self) -> List:
+        return self.x_train[-1:] + self.x_predict
 
-    def get_Y(self, a=None, b=None, N=None) -> List:
-        points_Y = []
-        if a is None and b is None:
-            points_X = self.get_X()
-        else:
-            points_X = self.get_X(a, b, N)
+    def get_Y_predict(self) -> List:
+        return self.y_train[-self.p - 1:] + self.y_predict
 
-        for x in points_X:
-            points_Y.append(np.sin(2 * np.sqrt(np.exp(x))))
+    def get_windows(self, x: List):
+        return [[x[i] for i in range(bias, bias + self.p)] for bias in range(len(x) - self.p)]
 
-        return points_Y
-
-    def get_delta(self, idx: int):
-        return self.real_values[idx] - self.learning_values[idx]
-
-    def get_epsilon(self):
-        epsilon = 0
-        for i in range(self.p, self.N):
-            delta = self.get_delta(i)
-            epsilon += delta ** 2
-        return np.sqrt(epsilon)
+    def get_predict_windows(self):
+        return [[self.y_train[i] for i in range(bias, bias + self.p)]
+                for bias in range(len(self.y_train) - self.p)]
 
     def learning(self):
-        self.real_values = self.get_Y()
-        self.learning_values = [0 for _ in range(len(self.real_values))]
+        windows = self.get_windows(self.y_train)
+        targets = self.y_train[self.p:]
         epsilon = 0
+        learning_values = []
         for epoch in range(self.number_of_epochs):
-            for point in range(self.p, self.N):
-                net = self.weights[self.p]
-                for i in range(self.p):
-                    net += self.weights[i] * self.real_values[point - self.p + i]
-                self.learning_values[point] = net
-                for k in range(self.p):
-                    self.weights[k] += self.learning_rate * self.get_delta(point) * self.real_values[point - self.p + k]
+            learning_values = []
+            total_error = 0
+            for (window, target) in zip(windows, targets):
+                x = [1] + window
+                out = sum([self.weights[i]*x[i] for i in range(len(self.weights))])
+                learning_values.append(out)
+                error = target - out
+                for i in range(0, len(self.weights)):
+                    self.weights[i] += self.learning_rate * error * x[i]
+                total_error += pow(error, 2)
+            epsilon = np.sqrt(total_error)
 
-            epsilon = self.get_epsilon()
-            if epsilon < 0.001:
-                print(epsilon)
-                break
+        print(epsilon)
+        print(self.weights)
 
-        print(f"Epoch: {self.number_of_epochs}, eps: {epsilon}, weights: {self.weights}")
-
-    def predict(self):
-        for i in range(self.N, 2 * self.N):
-            net = self.weights[self.p]
-            for j in range(self.p):
-                net += self.weights[j] * self.learning_values[i - self.p + j]
-            self.learning_values.append(net)
-
-    def plot_after_learning(self):
         plt.title("График после обучения")
         plt.xlabel("x")
         plt.ylabel("y")
         plt.grid()
-        X = self.get_X()
-        plt.plot(X, self.real_values, X[self.p:], self.learning_values[self.p:], 'ro')
+        x_train = self.x_train[self.p:]
+        plt.plot(self.x_train, self.y_train, x_train, learning_values, 'ro')
         plt.show()
 
-    def plot_predict(self):
+    def predict(self):
+        x_ = self.get_X_predict()
+        y = self.get_Y_predict()
+        windows = self.get_windows(y)
+        predictions = []
+        for window in windows:
+            x = [1] + window
+            prediction = sum([self.weights[i]*x[i] for i in range(len(self.weights))])
+            predictions.append(prediction)
+
+        y = y[self.p:]
         plt.title("График на следующем интервале")
         plt.xlabel("x")
         plt.ylabel("y")
         plt.grid()
-        X = self.get_X(self.a, self.b * 2 - self.a, self.N * 2)
-        Y = self.get_Y(self.a, self.b * 2 - self.a, self.N * 2)
-        plt.plot(X, Y, X[self.p:], self.learning_values[self.p:], 'ro')
+        plt.plot(x_, y, x_, predictions, 'ro')
         plt.show()
+
+    # def plot_after_learning(self):
+    #     plt.title("График после обучения")
+    #     plt.xlabel("x")
+    #     plt.ylabel("y")
+    #     plt.grid()
+    #     X = self.get_X()
+    #     plt.plot(X, self.real_values, X[self.p:], self.learning_values[self.p:], 'ro')
+    #     plt.show()
+
+    # def plot_predict(self):
+    #     plt.title("График на следующем интервале")
+    #     plt.xlabel("x")
+    #     plt.ylabel("y")
+    #     plt.grid()
+    #     X = self.get_X(self.a, self.b * 2 - self.a, self.N * 2)
+    #     Y = self.get_Y(self.a, self.b * 2 - self.a, self.N * 2)
+    #     plt.plot(X, Y, X[self.p:], self.learning_values[self.p:], 'ro')
+    #     plt.show()
